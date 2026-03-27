a4936af96d85e6c13b444b002b6ebe471c331cc3
murlloy <minguerilogenshin@gmail.com>
2026-03-27 08:19:06 -0300

    exercicio dia 26 resolvido

--- 26-03-exercicio/main.py
+++ 26-03-exercicio/main.py
@@ -3,25 +3,29 @@ qt_convidados = int(input("Digite a quantidade de convidados para sua festinha:\
 pessoas = 1
 
 pessoas_barradas = 0
+entradas = 0
 
 while pessoas <= qt_convidados:
-    idade_pessoa = int(input(f"Bem vindo {pessoas} pessoa!\nQual a sua idade:"))
-    if(idade_pessoa >= 18):
-        print("Pode entrar")
+    idade_pessoa = int(input(f"Bem vindo {pessoas} pessoa!\nQual a sua idade: "))
+
+    convite = input("Você possui o convite? sim ou não\n")
+    if(convite == "sim"):
+        convite = True
+        entradas += 1
     else:
-        convite = input("Você possui o convite? sim ou não")
-        if(convite == "sim"):
+        if(idade_pessoa>= 18):
             convite = True
+            entradas += 1
         else:
             convite = False
-        if(convite == True):
-            print("Você passou!")
-        else:
-            print("Você foi barrado pela segurança!")
-            pessoas_barradas += 1
+    if(convite == True):
+        print("\nVocê passou!\n")
+    else:
+        print("\nVocê foi barrado pela segurança!\n")
+        pessoas_barradas += 1
 
     pessoas += 1
     
 
-print(f"pessoas barradas {pessoas_barradas}")
-    
+print(f"pessoas barradas: {pessoas_barradas}")
+print(f"pessoas que entraram: {entradas}")
